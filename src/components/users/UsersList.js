27891e80bfad91e1408e8fbfa9e7da7eb8462545
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-06-26 01:48:44 -0400

    added drop down filters for users by sign

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -6,6 +6,9 @@ export const UserList = () => {
     const [profiles, setProfiles] = useState([])
 
     const [searchTerm, setSearchTerm] = useState("")
+    const [selectedSunSign, setSelectedSunSign] = useState("")
+    const [selectedMoonSign, setSelectedMoonSign] = useState("")
+    const [selectedRisingSign, setSelectedRisingSign] = useState("")
 
     useEffect(
         () => {
@@ -18,35 +21,101 @@ export const UserList = () => {
         []
     )
 
-    const filteredProfiles = profiles.filter((profile) =>
-        profile?.user.firstName.toLowerCase().startsWith(searchTerm.toLowerCase()) ||
-        profile?.user.lastName.toLowerCase().startsWith(searchTerm.toLowerCase())
-    )
+    const filteredProfiles = profiles.filter((profile) => {
+        const fullName = `${profile?.user.firstName} ${profile?.user.lastName}`.toLowerCase();
+        const nameMatch = fullName.startsWith(searchTerm.toLowerCase());
+        const sunSignMatch = selectedSunSign === "" || profile?.sun?.name.toLowerCase() === selectedSunSign.toLowerCase();
+        const moonSignMatch = selectedMoonSign === "" || profile?.moon?.name.toLowerCase() === selectedMoonSign.toLowerCase();
+        const risingSignMatch = selectedRisingSign === "" || profile?.rising?.name.toLowerCase() === selectedRisingSign.toLowerCase();
+
+        return (nameMatch || searchTerm === "") && sunSignMatch && moonSignMatch && risingSignMatch;
+    });
 
     const handleSearch = (event) => {
         setSearchTerm(event.target.value)
     }
 
+    const handleSunSignChange = (event) => {
+        setSelectedSunSign(event.target.value)
+    }
+
+    const handleMoonSignChange = (event) => {
+        setSelectedMoonSign(event.target.value)
+    }
+
+    const handleRisingSignChange = (event) => {
+        setSelectedRisingSign(event.target.value)
+    }
+
+
     return <>
         <h2>Celestial Users</h2>
-
-        <input
-            type="text"
-            placeholder="Search by name"
-            value={searchTerm}
-            onChange={handleSearch} />
+        <div>
+            <input
+                type="text"
+                placeholder="Search by name"
+                value={searchTerm}
+                onChange={handleSearch} />
+            <select value={selectedSunSign} onChange={handleSunSignChange}>
+                <option value="">All Sun Signs</option>
+                <option value="Aries">Aries</option>
+                <option value="Taurus">Taurus</option>
+                <option value="Gemini">Gemini</option>
+                <option value="Cancer">Cancer</option>
+                <option value="Leo">Leo</option>
+                <option value="Virgo">Virgo</option>
+                <option value="Libra">Libra</option>
+                <option value="Scorpio">Scorpio</option>
+                <option value="Sagittarius">Sagittarius</option>
+                <option value="Capricorn">Capricorn</option>
+                <option value="Aquarius">Aquarius</option>
+                <option value="Pisces">Pisces</option>
+            </select>
+            <select value={selectedMoonSign} onChange={handleMoonSignChange}>
+                <option value="">All Moon Signs</option>
+                <option value="Aries">Aries</option>
+                <option value="Taurus">Taurus</option>
+                <option value="Gemini">Gemini</option>
+                <option value="Cancer">Cancer</option>
+                <option value="Leo">Leo</option>
+                <option value="Virgo">Virgo</option>
+                <option value="Libra">Libra</option>
+                <option value="Scorpio">Scorpio</option>
+                <option value="Sagittarius">Sagittarius</option>
+                <option value="Capricorn">Capricorn</option>
+                <option value="Aquarius">Aquarius</option>
+                <option value="Pisces">Pisces</option>
+            </select>
+            <select value={selectedRisingSign} onChange={handleRisingSignChange}>
+                <option value="">All Rising Signs</option>
+                <option value="Aries">Aries</option>
+                <option value="Taurus">Taurus</option>
+                <option value="Gemini">Gemini</option>
+                <option value="Cancer">Cancer</option>
+                <option value="Leo">Leo</option>
+                <option value="Virgo">Virgo</option>
+                <option value="Libra">Libra</option>
+                <option value="Scorpio">Scorpio</option>
+                <option value="Sagittarius">Sagittarius</option>
+                <option value="Capricorn">Capricorn</option>
+                <option value="Aquarius">Aquarius</option>
+                <option value="Pisces">Pisces</option>
+            </select>
+        </div>
         <article className="users">
             {filteredProfiles.length > 0 ? (
                 filteredProfiles.map((profile) => (
                     <section className="user" key={`${profile.id}`}>
                         <header><Link to={`/Profile/${profile.id}`}>{profile?.user.firstName} {profile?.user.lastName}</Link></header>
-                        <div>Sun: {profile?.sun?.name}</div>
-                        <div>Moon: {profile?.moon?.name}</div>
-                        <div>Rising: {profile?.rising?.name}</div>
+                        <p>
+                            <div>Sun: {profile?.sun?.name}</div>
+                            <div>Moon: {profile?.moon?.name}</div>
+                            <div>Rising: {profile?.rising?.name}</div>
+                        </p>
                     </section>
                 ))
             ) : (
-                <p>No users found.</p>
+                <p>No matching users found.</p>
             )}
         </article>
     </>
